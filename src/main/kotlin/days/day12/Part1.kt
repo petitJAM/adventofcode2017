451aad248ac8bbfafa9e355c5a6e0f1d1a47faf1
Alex Petitjean <alex@tenforwardconsulting.com>
2017-12-21 11:31:56 -0600

    day 12 part 1

--- src/main/kotlin/days/day12/Part1.kt
+++ src/main/kotlin/days/day12/Part1.kt
@@ -3,6 +3,17 @@ package days.day12
 object Part1 {
 
     fun solve(puzzleInput: Map<Int, List<Int>>): Int {
-        TODO()
+
+        return addNeighbors(puzzleInput, listOf(0)).size
+    }
+
+    tailrec fun addNeighbors(allPrograms: Map<Int, List<Int>>, group: List<Int>): List<Int> {
+        val programsToAdd = group.flatMap { allPrograms[it] ?: emptyList() }.distinct()
+        val newGroup = (group + programsToAdd).distinct()
+        return if (newGroup == group) {
+            group
+        } else {
+            addNeighbors(allPrograms, newGroup)
+        }
     }
 }
